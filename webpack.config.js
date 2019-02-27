44674c7ab63a2950c9d6ccec3e3a45bc0b624934
TOSPUR\63081 <huochangsheng@tospur.com>
2019-02-27 15:17:11 +0800

    添加示例代码

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: 'production',  // "development" | "production" | "none"
+    devtool: 'inline-source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'easycomment.js',
+        library: 'easycomment',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "less-loader" }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|ttf|svg|woff|eot)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 30000
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify('production')
+            }
+        })
+    ]
+}
