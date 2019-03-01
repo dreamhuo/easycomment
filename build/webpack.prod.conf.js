53efd834905ed9c56096b74e7893f79c6bf779ac
TOSPUR\63081 <huochangsheng@tospur.com>
2019-03-01 15:16:31 +0800

    添加配置

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,3 +1,13 @@
 var config = require('../config');
 var baseWebpackConfig = require('./webpack.base.conf');
 const merge = require('webpack-merge');
+
+module.exports = merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
+});
