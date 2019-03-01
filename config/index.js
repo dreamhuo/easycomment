6e8742985a20889be1321f8b4e37530cdc5a6ebe
TOSPUR\63081 <huochangsheng@tospur.com>
2019-03-01 13:39:38 +0800

    添加配置

--- config/index.js
+++ config/index.js
@@ -1,5 +1,55 @@
+const path = require("path");
+const webpack = require("webpack");
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
-    build: {},
+    build: {
+        entry: "./src/index.js",
+        mode: 'production',  // "development" | "production" | "none"
+        devtool: 'inline-source-map',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'easycomment.js',
+            library: 'easycomment',
+            libraryTarget: 'umd'
+        },
+        module: {
+            rules:[
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        { loader: "style-loader" },
+                        { loader: "css-loader" },
+                        { loader: "less-loader" }
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.(png|jpg|gif|ttf|svg|woff|eot)$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 30000
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify('production')
+                }
+            }),
+            new VueLoaderPlugin()
+        ]
+    },
     dev: {},
     examplesDev: {
         env: require('./examples.dev.env'),
