aa92243e76da3a7852b9705f8e6598a249e5687d
TOSPUR\63081 <huochangsheng@tospur.com>
2019-03-01 16:38:14 +0800

    webpack开发配置

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,8 +2,9 @@ var config = require('../config');
 var baseWebpackConfig = require('./webpack.base.conf');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const webpackDevServer = require('webpack-dev-server');
 
-const config = merge(baseWebpackConfig, {
+const webpackConfig = merge(baseWebpackConfig, {
     mode: 'production',
     devtool: 'inline-source-map',
     plugins: [
@@ -15,11 +16,20 @@ const config = merge(baseWebpackConfig, {
     ]
 });
 
-webpack(config, (err, stats) => {
-  if (err || stats.hasErrors()) {
-    // 在这里处理错误
-    console.log(err);
-    return
-  }
-  console.log('处理完成！');
-});
+
+const options = {
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost'
+};
+
+// 想要启用 HMR，还需要修改 webpack 配置对象，使其包含 HMR 入口起点。
+// webpack-dev-server package 中具有一个叫做 addDevServerEntrypoints 的方法，你可以通过使用这个方法来实现
+webpackDevServer.addDevServerEntrypoints(webpackConfig, options);
+
+const compiler = webpack(webpackConfig);
+const server = new webpackDevServer(compiler, options);
+
+server.listen(5000, 'localhost', () => {
+    console.log('dev server listening on port 50000');
+})
