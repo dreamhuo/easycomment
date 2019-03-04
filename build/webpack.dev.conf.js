82211021074ab77e4d226aadc4bb7bb0c2dcd099
TOSPUR\63081 <huochangsheng@tospur.com>
2019-03-04 14:08:14 +0800

    配置开发服务器

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,14 +9,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const webpackConfig = merge(baseWebpackConfig, {
-    mode: 'production',
+    mode: 'development',
     entry: path.resolve(__dirname, '../src/index.js'),
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, '../dist'),
         publicPath: '/'
     },
-    devtool: 'inline-source-map',
     plugins: [
         new CleanWebpackPlugin(path.resolve(__dirname, '../dist'),{
             root: path.resolve(__dirname, '../'),
@@ -25,28 +24,21 @@ const webpackConfig = merge(baseWebpackConfig, {
         new HtmlWebpackPlugin({
             title: '开发环境'
         }),
-        new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             "process.env": {
-                NODE_ENV: JSON.stringify('production')
+                NODE_ENV: JSON.stringify('development')
             }
-        })
-    ]
+        }),
+        new webpack.HotModuleReplacementPlugin()    //引入热更新插件
+    ],
+    devtool: 'inline-source-map',
+    devServer:{
+        host:'localhost',
+        progress:true,
+        port:1573,
+        open:true,
+        hot:true
+    }
 });
 
-const options = {
-    contentBase: path.resolve(__dirname, '../dist'),
-    hot: true,
-    host: 'localhost'
-};
-
-// 想要启用 HMR，还需要修改 webpack 配置对象，使其包含 HMR 入口起点。
-// webpack-dev-server package 中具有一个叫做 addDevServerEntrypoints 的方法，你可以通过使用这个方法来实现
-webpackDevServer.addDevServerEntrypoints(webpackConfig, options);
-
-const compiler = webpack(webpackConfig);
-const server = new webpackDevServer(compiler, options);
-
-server.listen(5000, 'localhost', () => {
-    console.log('dev server listening on port 5000');
-})
+module.exports = webpackConfig;
